5d5e2656f0ee1abe51564a9055d26445c6365e20
Emmnia <eugmalikova@gmail.com>
2024-10-14 00:28:49 +0300

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
+import { faFaceFrownOpen } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { NavLink } from "react-router-dom"
 
 export const NotFound = () => {
     return (
         <div className="container">
-            <h1>Not Found</h1>
+            <h1><FontAwesomeIcon icon={faFaceFrownOpen} /> Not Found</h1>
             <NavLink to="/">To Home</NavLink>
         </div>
     )
