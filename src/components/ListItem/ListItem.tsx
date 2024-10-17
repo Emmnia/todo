699faa8924f2b77865b16ee1420f04c1c4ce26b5
Emmnia <eugmalikova@gmail.com>
2024-10-18 00:18:32 +0300

    link

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom"
 import { ToDo } from "../../models/todo-item"
 import classes from './ListItem.module.scss'
 
 export const ListItem = ({ todo }: { todo: ToDo }) => {
     return (
-        <a className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`} target="_blank" href={`/list/${todo.id}`} rel="noopener noreferrer"> {todo.text}</a >
+        <Link className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`} target="_blank" to={`/list/${todo.id}`} rel="noopener noreferrer"> {todo.text}</ >
     )
 }
