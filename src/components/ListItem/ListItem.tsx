e07497edd8d8503f83e6c6ca073c6ef1770cb0f1
Emmnia <eugmalikova@gmail.com>
2024-10-18 00:20:41 +0300

    link

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -4,6 +4,6 @@ import classes from './ListItem.module.scss'
 
 export const ListItem = ({ todo }: { todo: ToDo }) => {
     return (
-        <Link className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`} target="_blank" to={`/list/${todo.id}`} rel="noopener noreferrer"> {todo.text}</ >
+        <Link className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`} target="_blank" to={`/list/${todo.id}`} rel="noopener noreferrer"> {todo.text}</Link>
     )
 }
